0d5905f159168c5dce61e88f7faa18160ffbfe6b
Neetbear <52730691+Neetbear@users.noreply.github.com>
2022-05-13 17:43:26 +0900

    Revert "Merge branch 'njh' into dev"
    This reverts commit 2a36caec8257ef53ea668d82b412a5c98cd8dd4d, reversing
    changes made to 4b5bb28e28450f700421d7a3fcb9c1107403e89d.

--- back/server/routes/block/index.js
+++ back/server/routes/block/index.js
@@ -7,18 +7,5 @@ router.get('/getblock', blockController.getblock);
 
 router.post('/createBlock', blockController.createBlock);
 
-router.get('/blocks', blockController.blocks)
-
-router.get('/latestBlock', blockController.latestBlock)
-
-router.post('/miningBlock', blockController.miningBlock)
-
-router.post('/autoMiningBlock', blockController.autoMiningBlock)
-
-router.get('/log', blockController.log)
-
-router.get('/createWallet', blockController.getWallet)
-
-router.post('/createWallet', blockController.setWallet)
 
 module.exports = router;
