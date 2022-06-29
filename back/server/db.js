994078178740835037be6d40a02507086e01eef7
Neetbear <52730691+Neetbear@users.noreply.github.com>
2022-06-29 16:55:47 +0900

    Update db.js

--- back/server/db.js
+++ back/server/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-    host : '3.80.201.98',
-    user : 'sieun',
-    password : 'Enffl5962!',
-    database : 'blockchain'
+    host : '127.0.0.1',
+    user : 'root',
+    password : '85354351',
+    database : 'block'
 })
 
 module.exports = pool;
