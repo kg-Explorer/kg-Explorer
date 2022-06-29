298c0d63f7b92b7fe3c985cc8128357c3c7d9b79
Neetbear <52730691+Neetbear@users.noreply.github.com>
2022-06-29 16:55:24 +0900

    Update block.sql

--- back/block.sql
+++ back/block.sql
@@ -1,8 +1,5 @@
 
 -- blocks SQL
-DROP TABLE blocks;
-
-
 CREATE TABLE blocks(
     `blockIndex` INT(11) NOT NULL,
     `data` VARCHAR(255) NOT NULL,
@@ -13,19 +10,13 @@ CREATE TABLE blocks(
     `nonce` INT(11) NOT NULL,
     `miner` VARCHAR(255) NOT NULL
 );
+DROP TABLE blocks;
 SELECT * FROM blocks;
 DESC blocks;
 USE blockchain;
-
-
 INSERT INTO blocks(blockIndex, data, timestamp, hash, previousHash, difficulty, nonce, miner) VALUES('0', "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks", '1652259600', 'e8a80e694c0e1e18fc170a00acb10dad2e2fa7e992e7085980e5609c313bfa45', '0', '1', '0', 'sieun');
 
-
-
-
 -- wallet SQL
-DROP TABLE wallet;
-
 CREATE TABLE wallet(
     `idx` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `publicKey` VARCHAR(255) NOT NULL,
@@ -33,12 +24,11 @@ CREATE TABLE wallet(
     `walletAmount` INT(11) NOT NULL
 );
 
+DROP TABLE wallet;
 SELECT * FROM wallet;
 DESC wallet;
 
-
 -- tx table
-
 CREATE TABLE tx(
     `txFrom` VARCHAR(255) NOT NULL,
     `txTo` VARCHAR(255) NOT NULL,
@@ -46,12 +36,6 @@ CREATE TABLE tx(
     `txTime` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-SELECT * FROM tx;
-
 DROP TABLE tx;
-
-
-
-
-
-
+SELECT * FROM tx;
+DESC tx
